7176f2bd0b187cc4b685dfd2265ab02e3fed2ed1
Exile <csxkingjp@gmail.com>
2021-02-28 12:38:07 -0700

    Added NCS.funct.unload() to completely unload NCS, begain adding changelog and init

--- ncs.js
+++ ncs.js
@@ -17,13 +17,60 @@ window.onload = function() {
     }
 }
 
-const NCS = {
+var NCS = {
     // all code goes here, to prevent conflicts with Musiqpad or other scripts.
     settings: {
-    	rcs: {
-    		rcsThemeJson: {"room":"Radiant Music","author":"Origin","icon":"https://cdn.radiant.dj/rcs/icons/radiant/logo.png","css":"https://cdn.radiant.dj/rcs/rs_room.css","rules":{"allowAutorespond":true,"allowAutowoot":true,"allowAutojoin":true,"allowAutograb":true,"allowSmartVote":true,"allowEmotes":true,"allowShowingMehs":true,"forceSmartVote":false},"ccc":{"admin":"#42A5DC","ambassador":"#84B423","host":"#FF9B00","cohost":"#F3508F","manager":"#F3508F","bouncer":"#F3508F","rdj":"#5DB4FF","subscriber":"#A1A6B3","silversubscriber":"#A1A6B3","friend":"#B35D88","user":"#8B92A3"},"images":{"background":null,"playback":null,"admin":"https://cdn.radiant.dj/rcs/icons/radiant/admin.png","ambassador":"https://cdn.radiant.dj/rcs/icons/radiant/ambassador.png","host":"https://cdn.radiant.dj/rcs/icons/radiant/host.png","cohost":"https://cdn.radiant.dj/rcs/icons/radiant/cohost.png","manager":"https://cdn.radiant.dj/rcs/icons/radiant/manager.png","bouncer":"https://cdn.radiant.dj/rcs/icons/radiant/bouncer.png","rdj":"https://cdn.radiant.dj/rcs/icons/radiant/residentdj.png","subscriber":null,"silversubscriber":"https://cdn.radiant.dj/rcs/icons/radiant/subscriber.png"},"autocomplete":[],"emotes":{"custom_test":"https://cdn.radiant.dj/rcs/icons/test_custom.png"}},
-    		
-    	}
+        version: "2.0.0",
+        changelog: ""
+        rcs: {
+            rcsThemeJson: {
+                "room": "Radiant Music",
+                "author": "Origin",
+                "icon": "https://cdn.radiant.dj/rcs/icons/radiant/logo.png",
+                "css": "https://cdn.radiant.dj/rcs/rs_room.css",
+                "rules": {
+                    "allowAutorespond": true,
+                    "allowAutowoot": true,
+                    "allowAutojoin": true,
+                    "allowAutograb": true,
+                    "allowSmartVote": true,
+                    "allowEmotes": true,
+                    "allowShowingMehs": true,
+                    "forceSmartVote": false
+                },
+                "ccc": {
+                    "admin": "#42A5DC",
+                    "ambassador": "#84B423",
+                    "host": "#FF9B00",
+                    "cohost": "#F3508F",
+                    "manager": "#F3508F",
+                    "bouncer": "#F3508F",
+                    "rdj": "#5DB4FF",
+                    "subscriber": "#A1A6B3",
+                    "silversubscriber": "#A1A6B3",
+                    "friend": "#B35D88",
+                    "user": "#8B92A3"
+                },
+                "images": {
+                    "background": null,
+                    "playback": null,
+                    "admin": "https://cdn.radiant.dj/rcs/icons/radiant/admin.png",
+                    "ambassador": "https://cdn.radiant.dj/rcs/icons/radiant/ambassador.png",
+                    "host": "https://cdn.radiant.dj/rcs/icons/radiant/host.png",
+                    "cohost": "https://cdn.radiant.dj/rcs/icons/radiant/cohost.png",
+                    "manager": "https://cdn.radiant.dj/rcs/icons/radiant/manager.png",
+                    "bouncer": "https://cdn.radiant.dj/rcs/icons/radiant/bouncer.png",
+                    "rdj": "https://cdn.radiant.dj/rcs/icons/radiant/residentdj.png",
+                    "subscriber": null,
+                    "silversubscriber": "https://cdn.radiant.dj/rcs/icons/radiant/subscriber.png"
+                },
+                "autocomplete": [],
+                "emotes": {
+                    "custom_test": "https://cdn.radiant.dj/rcs/icons/test_custom.png"
+                }
+            },
+
+        }
     },
     funct: {
         addMenu: (function() {
@@ -71,10 +118,35 @@ const NCS = {
                 $compile($('#app-right > #ncs-back'))(scope);
                 scope.$apply();
             });
-        })
-    },
-    init: function() {
+        }),
+        chatMsg: function(message) {
+            var dt = new Date();
+            var time = dt.getHours() + ":" + dt.getMinutes();
+            $('#messages').append('<div id="cm-34" style="background-color:purple;" class="cm message self"><span class="time">' + time + '</span><svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" version="1.1" class="bdg  hidden">\
+        <defs>\
+            <linearGradient id="badgegrad;#ff0000;#4800ff;">\
+                <stop stop-color="#ff0000" offset="49%"></stop>\
+                <stop stop-color="#4800ff" offset="51%"></stop>\
+            </linearGradient>\
+        </defs>\
+        <g>\
+            <circle id="circle" r="7.25" cy="8" cx="8" transform="rotate(45, 8, 8)" stroke-linecap="null" stroke-linejoin="null" stroke="#ffffff" fill="url(#badgegrad;#ff0000;#4800ff;)" stroke-width="1.5"></circle>\
+        </g>\
+    </svg>\
+    <div class="text"><span class="umsg">' + message + '</span></div>\
+</div>');
+        },
+        unload: function() {
+            $("[id^=NCS").remove();
+            NCS = null;
+            this.chatMsg('Unloaded NCS, you may have to refresh to reload it.');
 
+        },
+    },
+
+    init: function() {
+        var onLoadMsg = "NCS version " + NCS.settings.version + " loaded successfully!";
+        
 
     }
 };
