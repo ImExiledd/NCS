5c449b2b39d384e506bc7f844a66a4b633e0ba9f
toys0125 <marcus.g.stange@gmail.com>
2021-03-01 16:04:31 -0600

    Worked first try.

--- ncs.js
+++ ncs.js
@@ -6,7 +6,7 @@
  *
  */
 // ensure jquery works
-window.onload = function() {
+window.onload = function () {
     if (!window.jquery) {
         var script = document.createElement('script');
         script.type = "text/javascript";
@@ -18,7 +18,7 @@ window.onload = function() {
     setTimeout(NCS.init(), 1500);
 }
 
-window.onbeforeunload = function(e) {
+window.onbeforeunload = function (e) {
     // save ncs settings
     window.localStorage.setItem('ncs2-settings', JSON.stringify(NCS.userSettings));
 }
@@ -26,8 +26,8 @@ window.onbeforeunload = function(e) {
 var NCS = {
     // all code goes here, to prevent conflicts with Musiqpad or other scripts.
     userSettings: $.extend({
-        autolike: false,
-        autojoin: false,
+        autoLike: false,
+        autoJoin: false,
         eta: true,
         customBackground: false,
         customBackgroundUri: null,
@@ -37,7 +37,7 @@ var NCS = {
     }, (JSON.parse(window.localStorage.getItem('ncs2-settings')) || {})),
     settings: {
         version: "2.0.0",
-        changelog: $.getJSON("https://cdn.jsdelivr.net/gh/ImExiledd/NCS@new/changelog.json", function(json){console.info("found changelog");/*NCS.settings.changelog= json;*/}),
+        changelog: $.getJSON("https://cdn.jsdelivr.net/gh/ImExiledd/NCS@new/changelog.json", function (json) { console.info("found changelog");/*NCS.settings.changelog= json;*/ }),
         rcs: {
             rcsThemeJson: {
                 "room": "Radiant Music",
@@ -89,7 +89,7 @@ var NCS = {
         }
     },
     funct: {
-        addMenu: (function() {
+        addMenu: (function () {
             var models = {
                 'tab': `<div id="NCSMenu" data-ng-click="prop.c = 31" data-ng-class="{\'active\' : prop.c == 31}" class="tab ncs-tab">
                     <span class="icon-info">NCS</span>
@@ -97,8 +97,8 @@ var NCS = {
                 'back': `<div data-ng-show="(prop.c == 31)" class="ng-hide" id="ncs-back">
                     <div class="items">
                         <div id="header-general" class="header">General Functionality</div>
-                        <div id="auto-like" class="item auto-like">AutoLike</div>
-                        <div id="auto-join" class="item auto-join" onclick='NCS.funct.autoJoinCheck(true)'>AutoJoin DJ Queue</div>
+                        <div id="autoLike" class="item auto-like">AutoLike</div>
+                        <div id="autoJoin" class="item auto-join" onclick='NCS.funct.settingChanger('autoJoin')'>AutoJoin DJ Queue</div>
                         <div id="afk-responder" class="item afk-responder">AFK Responder</div>
                         <div id="header-themes" class="header">Themes</div>
                         <div id='custom-theme' class='item mqp-rcs' onclick='NCS.funct.setTheme("rcs");'>RCS Theme Revived</div>
@@ -109,7 +109,7 @@ var NCS = {
                         <div id="desktop-notifs" class="item desktop-notifs" onclick='toggleDesktopNotifications();'>Desktop Notifications</div>
                         <div id="custom-background" class="item custom-background">Custom Background</div>
                         <div id="custom-mention-sounds" class="item custom-mention-sounds">Custom Mention Sounds</div>
-                        <div id="eta" class="item eta">ETA</div>
+                        <div id="eta" class="item eta" onclick='NCS.funct.settingChanger('eta')'>ETA</div>
                         <div id="header-moderation" class="header">Moderation</div>
                         <div id="moderatorSongDurationAlert" class="item eta">Song Duration Alert</div>
                         <div id="header-edit-stuff" class="header">Edit your Settings</div>
@@ -130,65 +130,101 @@ var NCS = {
 
             // Apply shit to the scope
             var scope = angular.element('.tray > *').scope();
-            $('body').injector().invoke(function($compile, $rootScope) {
+            $('body').injector().invoke(function ($compile, $rootScope) {
                 $compile($('.dash .tray .ncs-tab'))(scope);
                 $compile($('#app-right > #ncs-back'))(scope);
                 scope.$apply();
             });
         }),
         intervals: {
-            readableEta: function(total) {
+            readableEta: function (total) {
                 var hours = ~~(total / 3600);
                 var minutes = (~~(total / 60)) % 60;
                 var seconds = total % 60
                 return NCS.funct.intervals.normalize(hours) + ':' + NCS.funct.intervals.normalize(minutes) + ':' + NCS.funct.intervals.normalize(seconds);
             },
-            normalize: function(number) {
+            normalize: function (number) {
                 var addition = (number < 10
                     ? '0'
                     : '');
-                    return addition + number;
+                return addition + number;
             },
-            eta: setInterval(function() {
+            eta: setInterval(function () {
                 var position = API.queue.getPosition();
-                position = (position < 0) ? API.queue.getDJs().length : position-1;
+                position = (position < 0) ? API.queue.getDJs().length : position - 1;
                 var eta = ~~((position * (3.5 * 60)) + API.room.getTimeRemaining());
-                if(position == -1 && NCS.userSettings.autojoin){
+                if (position == -1 && NCS.userSettings.autoJoin) {
                     NCS.funct.intervals.autojoin();
                 }
-                if(NCS.userSettings.eta) {
+                if (NCS.userSettings.eta) {
                     // true
-                    if(API.queue.getPosition() === 0) {
+                    if (API.queue.getPosition() === 0) {
                         $('.btn-join').attr('data-eta', "You're the DJ!");
                     } else {
                         $('.btn-join').attr('data-eta', NCS.funct.intervals.readableEta(eta));
                     }
                 }
             }, 1000),
-            autojoin: function(){
+            autojoin: function () {
                 var permission = (typeof permissions === "undefined") ? API.util.hasPermission('djqueue.join') : permission
-                if (API.queue.getPosition() == -1 && permission){
+                if (API.queue.getPosition() == -1 && permission) {
                     API.queue.join()
                 } else {
                     return;
                 }
             }
         },
-        autoJoinCheck: function(setsettings){
-            if (setsettings){
-                NCS.userSettings.autojoin = !NCS.userSettings.autojoin;
-            }
-            if (NCS.userSettings.autojoin){
-                $('auto-join').addClass('active');
+        checkMarkSetting: function () {
+            /*
+                    autoLike: false,
+                    autoJoin: false,
+                    eta: true,
+                    customBackground: false,
+                    customBackgroundUri: null,
+                    customThemeEnabled: false,
+                    moderatorSongAlert: false,
+                    currentTheme: null,
+                    */
+            if (NCS.userSettings.autoLike) {
+                NCS.funct.checkMarkChanger('autoLike');
             } else {
-                $('auto-join').removeClass('active');
+                NCS.funct.checkMarkChanger('autoLike', false)
+            }
+            if (NCS.userSettings.autoJoin) {
+                NCS.funct.checkMarkChanger('autoJoin');
+            } else {
+                NCS.funct.checkMarkChanger('autoJoin', false);
+            }
+            if (NCS.userSettings.eta) {
+                NCS.funct.checkMarkChanger('eta');
+            } else {
+                NCS.funct.checkMarkChanger('eta');
+            }
+            if (NCS.userSettings.currentTheme) {
+                NCS.funct.checkMarkChanger('customBackground');
+                NCS.funct.setTheme(NCS.userSettings.currentTheme)
             }
         },
-        chatMsg: function(message, classname) {
+        settingChanger: function (setting, state) {
+            if (typeof state === "undefined") {
+                NCS.userSettings[setting] = !NCS.userSettings[setting];
+            } else {
+                NCS.userSettings[setting] = state;
+            }
+
+        },
+        checkMarkChanger: function (setting, state) {
+            if ($('#' + setting).hasClass('active')) {
+                $('#' + setting).removeClass('active');
+            } else if (!state) {
+                $('#' + setting).addClass('active');
+            }
+        },
+        chatMsg: function (message, classname) {
             var dt = new Date();
             var time = dt.getHours() + ":" + dt.getMinutes();
             $('#messages').append('\
-            <div id="cm-34" style="" class="cm '+ classname +' message self"><span class="time">' + time + '</span><svg viewBox="0 0 16 16" xmlns="https://www.w3.org/2000/svg" version="1.1" class="bdg  hidden">\
+            <div id="cm-34" style="" class="cm '+ classname + ' message self"><span class="time">' + time + '</span><svg viewBox="0 0 16 16" xmlns="https://www.w3.org/2000/svg" version="1.1" class="bdg  hidden">\
                     <defs>\
                         <linearGradient id="badgegrad;#ff0000;#4800ff;">\
                             <stop stop-color="#ff0000" offset="49%"></stop>\
@@ -203,8 +239,8 @@ var NCS = {
             </div>');
         },
         previousThemeName: null,
-        loadCount: (typeof loadCount === "undefined") ? 0 :this.funct.loadCount,
-        setTheme: function(themeName) {
+        loadCount: (typeof loadCount === "undefined") ? 0 : this.funct.loadCount,
+        setTheme: function (themeName) {
             var themeURI = "https://get.imexile.moe/NCS/themes/" + themeName + ".css";
             $('#NCSTheme').remove();
             $('head').append("<link id='NCSTheme' rel='stylesheet' href='" + themeURI + "' />");
@@ -215,7 +251,7 @@ var NCS = {
             // end set active
             this.previousThemeName = themeName;
         },
-        unload: function() {
+        unload: function () {
             $("[id^=NCS").remove();
             $('#ncs-back').remove();
             $('style[class=NCSSTYLE]').remove();
@@ -226,18 +262,18 @@ var NCS = {
 
         },
     },
-    sleep: function(milliseconds){
+    sleep: function (milliseconds) {
         const date = Date.now();
         let currentDate = null;
         do {
-          currentDate = Date.now();
+            currentDate = Date.now();
         } while (currentDate - date < milliseconds);
     },
-    waitload:function(){
-        if(!NCS.settings.changelog.responseJSON){
+    waitload: function () {
+        if (!NCS.settings.changelog.responseJSON) {
             console.debug(NCS.settings.changelog)
             console.debug(Object.keys(NCS.settings.changelog))
-            if(NCS.funct.loadCount >1){
+            if (NCS.funct.loadCount > 1) {
                 console.error("Failed to load")
                 this.funct.chatMsg("Failed to load changelog");
                 this.funct.unload;
@@ -250,25 +286,21 @@ var NCS = {
         }
     },
 
-    init: function() {
+    init: function () {
         // Changelog json stuffs:
         // changelog.title changelog.tagline changelog.html
         // begin init
-        if(this.waitload() == 'exit'){
+        if (this.waitload() == 'exit') {
             delete this;
         }
         NCS.funct.addMenu();
         $('head').append('<link rel="stylesheet" class="NCS" href="https://get.imexile.moe/NCS/ncs.css" />');
         $('head').append('<link rel="stylesheet" id="NCSTheme" href="" />');
-        // load specific settings
-        if (NCS.userSettings.currentTheme){
-            NCS.funct.setTheme(NCS.userSettings.currentTheme);
-        }
-        NCS.funct.autoJoinCheck();
+        NCS.funct.checkMarkSetting();
         // do this after init success
         var onLoadMsg = "NCS version " + NCS.settings.version + " loaded successfully!";
         // Make sure that changelog loaded
-        
+
         var changelog = NCS.settings.changelog.responseJSON;
         NCS.funct.chatMsg("NCS v" + NCS.settings.version + " loaded! | " + changelog.title, "NCSMSG NCSMSG-TITLE");
         NCS.funct.chatMsg(changelog.tagline, "NCSMSG-TAGLINE");
@@ -287,6 +319,6 @@ var NCS = {
             font-style:italic;\
         }\
         </style>');
-        
+
     }
 };
